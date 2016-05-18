a4656822c9c9260991ee323500bb0d226abc9a62
André Matias <andrersmatias@gmail.com>
2016-05-18 18:45:11 -0300

    Controller-login.php
    1 - Incluindo comentários para documentação
    2 - Definindo propriedades e métodos para controle do painel de Login.

--- App/core/controllers/controller-login.php
+++ App/core/controllers/controller-login.php
@@ -1,10 +1,60 @@
 <?php
 /**
-* Classe para chamar e controlar o painel de login
+* Classe para controlar as ações no painel de Login
+* @author: André Matias
+* @version: 0.1
+* @link github.com/Andrematias
+* @link andrersmatias@gmail.com
 */
-class Login{
-	
-	function Index(){
-		echo 'Chamada de View para o painel';
+class Login extends MainController{
+
+	/**
+	* _viewPainel propriedade para o Obj da classe que renderiza o painel na 
+	* tela.
+	*/
+	public $_viewPainel;
+
+	/**
+	* _modelLogin propriedade para o Obj da classe de modelagem dos dados 
+	* obtidos pelo painel.
+	*/
+	public $_modelLogin;
+
+	public $_params = [];
+
+	/**
+	* _404 propriedade para chamar a página em caso de alguma falha
+	*/
+	public $_404 = '_includes/404.html';
+
+
+	/**
+	* Método padrão da classe implementa a view do painel, modelagem dos dados
+	* e redirecionadores
+	*/
+	public function Index(){
+		if(file_exists(ROOT.'/App/core/views/view-painel.php')){
+			require_once(ROOT.'/App/core/views/view-painel.php');
+			require_once(ROOT.'/App/core/models/model-painelLogin.php');
+			$this->_viewPainel = new ViewLogin();
+			$this->_modelLogin = new ModelLogin();
+
+			//Verificar se existe o usuário 
+			// $this->_modelLogin->CheckLogin();
+			@$this->_params = [$_POST['user'], $_POST['pass']];
+			$this->_logged = call_user_func_array([$this->_modelLogin, 'CheckUser'], @$this->_params);
+
+			//definir as variaveis de sessão
+			if($this->_logged == true){
+				$this->_modelLogin->setSessionVars();
+			}
+
+			//Redireciona o usuário para a página após o login
+			if(parent::CheckSession() == true){
+				header('Location:'.SITEPATH);
+			}
+		}else{
+			include_once(TEMPLATES.$this->_404);
+		}
 	}
 }
