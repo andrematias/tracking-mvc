105c0abe416237e7f5c76b088babccb37dc15619
André Matias <andrersmatias@gmail.com>
2016-05-18 18:50:39 -0300

    MainController - Inclusão de método para checar sessão ativa

--- App/lib/mainController.class.php
+++ App/lib/mainController.class.php
@@ -3,7 +3,17 @@
 	* Executa métodos globais para todas os controllers
 	*/
 	class MainController extends UserLogin{
+
 		function __construct(){
-			// parent::__construct();
+			parent::InitSession();
+		}
+
+		/*Checar se existe uma sessão logada*/
+		protected function CheckSession(){
+			if(isset($_SESSION['user']) && isset($_SESSION['pass'])){
+				return $this->_logged = true;
+			}else{
+				return false;
+			}
 		}
 	}
