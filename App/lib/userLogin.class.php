f7a2b7d6a51fe78df596d7106041aed4476cb779
André Matias <andrersmatias@gmail.com>
2016-05-18 18:54:10 -0300

    Alteraçãoes na estrutura e estetica da Classe userLogin
    1- Adicionando comentários para documentação
    2 - Criação de métodos InitSession() e SessionDestroy()

--- App/lib/userLogin.class.php
+++ App/lib/userLogin.class.php
@@ -4,9 +4,31 @@
 	* verifica se existe alguma sessão iniciada, gerencia cookies.
 	*/
 	class UserLogin	{
+
+		protected $_logged = false;
 		
-		function initSession(){
-			session_start();
-			$_SESSION['user'] = 'Matias';
+		/**
+		* Método para ativar o valor das variaeis $_SESSION em todas
+		* as páginas que ela estiver presente.
+		*/
+		protected function InitSession(){
+			if(session_id() == null){
+				ini_set('session.name', 'tracking_session');
+				ini_set('session.hash_function', 'sha512');
+				if(is_writable(session_save_path()) != 1 ) session_save_path('/tmp');
+				session_start();
+				session_regenerate_id(true);
+			}
+		}
+		
+
+		/**
+		* Método para destruir uma sessão e seus valores
+		*/
+		protected function SessionDestroy(){
+			if(session_id() != null){
+				$_SESSION = array();
+				session_destroy();
+			}
 		}
 	}
