4e714e9108b9093ebac319d4b899990871f174e7
André Matias <andrersmatias@gmail.com>
2016-05-18 18:55:37 -0300

    Alterações no arquivo de configuração da App
    Inclusão de Constantes

--- config.php
+++ config.php
@@ -1,6 +1,20 @@
 <?php
 //Diretórios
 	define('ROOT', dirname(__FILE__));
+	define('TEMPLATES', ROOT.'/App/core/views/templates/');
+
+//SITEPATH
+	define('PROTOCOL', (isset($_SERVER['HTTPS'])) ? 'https://' : 'http://');
+	define('DOMAIN', $_SERVER['HTTP_HOST']);
+	define('SITEPATH', PROTOCOL.DOMAIN);
 
 //Debug mode (Para desativar notificações e mensagens de erros mude para false).
 	define('DEBUG', true);
+
+// Dados de acesso ao banco de dados.
+	define('DB_HOSTNAME', '127.0.0.1');
+	define('DB_NAME', 'test');
+	define('DB_USER', 'root');
+	define('DB_DRIVER', 'mysql');
+	define('DB_PASS', '');
+	define('DB_CHARSET', 'charset=utf8');
