55336fcfa37fb3161043fd7aab23f067bfa569ec
André Matias <andrersmatias@gmail.com>
2016-05-18 18:43:29 -0300

    Verificando se existe uma sessão aberta no controller Home

--- App/core/controllers/controller-home.php
+++ App/core/controllers/controller-home.php
@@ -4,10 +4,13 @@
 	*/
 	class Home extends MainController{
 		
-		function index(){
+		function Index(){
 			/*Chama o view para mostrar a página inicial da App se houver 
 			uma sessão iniciada verificada pelo método da parent que herda 
 			da parent dela */
+			if(parent::CheckSession() != TRUE){
+				header('Location:'.SITEPATH.'/login');
+			}
 			echo 'HOME METODO INDEX ';
 		}
 	}
