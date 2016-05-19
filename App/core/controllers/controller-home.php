f1fd7646c09fdfa7c6f2553a44c6ba4a48db5d0c
André Matias <andrersmatias@gmail.com>
2016-05-19 19:01:41 -0300

    Controller - Home
    1 - Criação dos métodos da classe controladora da home.

--- App/core/controllers/controller-home.php
+++ App/core/controllers/controller-home.php
@@ -3,6 +3,8 @@
 	* Controler inicial
 	*/
 	class Home extends MainController{
+
+		public $_home_view;
 		
 		function Index(){
 			/*Chama o view para mostrar a página inicial da App se houver 
@@ -11,6 +13,38 @@
 			if(parent::CheckSession() != TRUE){
 				header('Location:'.SITEPATH.'/login');
 			}
-			echo 'HOME METODO INDEX ';
+			// if(isset($_REQUEST['go']) && $_REQUEST['go'] == 'loggout'){
+			// 	parent::SessionDestroy();
+			// }
+			$this->redirect();
+
+			//Verifica e chama a view da home
+			if(file_exists(ROOT.'/App/core/views/view-home.php')){
+				require_once(ROOT.'/App/core/views/view-home.php');
+				$this->_home_view = new ViewHome();
+				$this->_home_view->ShowHome();
+			}else{
+				header('Location:'.SITEPATH.'/pageNotFound');
+			}
+			// echo 'HOME METODO INDEX <a href="'.SITEPATH.'?go=loggout">Sair</a>';
+		}
+
+
+		protected function redirect(){
+			$go = (isset($_REQUEST['go'])) ? $_REQUEST['go'] : NULL;
+
+			switch ($go) {
+				case 'loggout':
+					parent::SessionDestroy();
+					break;
+
+				case 'configuracoes':
+					header('Location:'.SITEPATH.'/configuracoes');
+					break;					
+
+				case 'home':
+					header('Location:'.SITEPATH);
+					break;					
+			}
 		}
 	}
