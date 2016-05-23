ebf1b292b832e9a9807895340df7a9bdc7592095
André Matias <andrersmatias@gmail.com>
2016-05-23 14:40:29 -0300

    Controller Home
    1 - Movendo o método redirect para o contruction da Main Controller.
    
    2- Removendo a sujeira de comentários

--- app/core/controllers/controller-home.php
+++ app/core/controllers/controller-home.php
@@ -13,10 +13,6 @@
 			if(parent::CheckSession() != TRUE){
 				header('Location:'.SITEPATH.'/login');
 			}
-			// if(isset($_REQUEST['go']) && $_REQUEST['go'] == 'loggout'){
-			// 	parent::SessionDestroy();
-			// }
-			$this->redirect();
 
 			//Verifica e chama a view da home
 			if(file_exists(ROOT.'/app/core/views/view-home.php')){
@@ -25,26 +21,6 @@
 				$this->_home_view->ShowHome();
 			}else{
 				header('Location:'.SITEPATH.'/pageNotFound');
-			}
-			// echo 'HOME METODO INDEX <a href="'.SITEPATH.'?go=loggout">Sair</a>';
-		}
-
-
-		protected function redirect(){
-			$go = (isset($_REQUEST['go'])) ? $_REQUEST['go'] : NULL;
-
-			switch ($go) {
-				case 'loggout':
-					parent::SessionDestroy();
-					break;
-
-				case 'configuracoes':
-					header('Location:'.SITEPATH.'/configuracoes');
-					break;					
-
-				case 'home':
-					header('Location:'.SITEPATH);
-					break;					
 			}
 		}
 	}
