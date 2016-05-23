1de147b6edc43c70238ceb28b8fdf9e082362ab3
André Matias <andrersmatias@gmail.com>
2016-05-23 14:41:05 -0300

    Load.php
    Adicionando comentários para documentação

--- load.php
+++ load.php
@@ -1,6 +1,10 @@
 <?php
 	require_once('config.php');
 
+	/**
+	* Verifica se o modo Debug esta ativo
+	*/
+
 	if(DEBUG === false){
 		ini_set('display_errors', 'OFF');
 		error_reporting(0);
@@ -9,6 +13,11 @@
 		error_reporting(E_ALL);
 	}
 
+	/**
+	* Carrega as classes automaticamente com a spl_autoload_register('__load');
+	* @param __load nome da classe que sera carregado, atribuido automaticamente
+	* quando instanciado uma classe.
+	*/
 	if(!function_exists('__load')){
 		function __load($calssName){
 			$file_path = strtolower(ROOT.'/app/lib/'.$calssName.'.class.php');
