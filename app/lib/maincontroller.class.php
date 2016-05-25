aedb83709e6f4918b005e7e346bd5a30ebc34a7a
André Matias <andrersmatias@gmail.com>
2016-05-25 17:56:03 -0300

    Inclusão de método newMessage nas classes MainModel e MainController

--- app/lib/maincontroller.class.php
+++ app/lib/maincontroller.class.php
@@ -23,4 +23,23 @@
 				return false;
 			}
 		}
+
+		/**
+		* Método para adicionar mensagem ao buffer
+		* @param msg, string mensagem para ser adicionada
+		* @param type, string success ou error 
+		*/
+		public function newMessage($msg, $type){
+			global $msgPool;
+			if(!is_array($msgPool)){
+				$msgPool = array();
+			}
+
+			$messages = array(
+				'msg'  => $msg,
+				'type' => $type
+			);
+
+			array_push($msgPool, $messages);
+		}
 	}
