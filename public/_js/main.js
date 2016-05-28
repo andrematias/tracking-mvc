88e7f16fb04b437ead23d2c41dafff30e6739b1e
André Matias <andrersmatias@gmail.com>
2016-05-28 17:26:24 -0300

    main.js remove mensagens on click or setInterval in 10 seconds

--- public/_js/main.js
+++ public/_js/main.js
@@ -1,5 +1,11 @@
-$(function(){
+$(document).ready(function(){
+	//Remove a mensagem ao clicar
+	$('[id=type_message]').click(function(){
+		$(this).remove();
+	});
+
+	//Remove as mensagens apos 10 segundos
 	setInterval(function(){
-		$('[id=type_message]').hide();
-	}, 5000);
+		$('[id=type_message]').remove();
+	}, 10000);
 });
