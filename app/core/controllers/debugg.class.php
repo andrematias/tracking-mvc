585430a5b94601b550ddcb0556ee7cb4526ee0b2
Andratias <andrersmatias@gmail.com>
2016-06-24 18:45:34 -0300

    Novos Debugs e testes

--- app/core/controllers/debugg.class.php
+++ app/core/controllers/debugg.class.php
@@ -4,30 +4,27 @@
 	*/
 	namespace App\Core\Controllers;
 	use App\Lib\DbTrack AS Banco;
-	use \PDO;
+        use App\Lib\TrackingAPI as API;
+        use App\Lib\Cliente;
+        use App\Lib\User;
 
 	class Debugg extends Banco
 	{
 		
 		function index()
 		{
-			/*$delete = parent::Delete
-			(
-				'tr_user',
-				"WHERE cliente = ?",
-				['invalido']
-			);
+                    $arr = ['cliente' => 'blog.com'];
 
-			var_dump($delete);*/
-			var_dump
-			(
-				$db = parent::Select
-				(
-					'tr_user', 
-					['cliente'], 
-					'WHERE cliente LIKE ? LIMIT 10', 
-					['%invalido%']
-				)
-			);
+                    $keys = array_keys( $arr );
+                    $colunas = implode(', ', $keys);
+                    $campos = ':'.implode(', :', $keys);
+                    $valores = array_values($arr);
+
+                    //var_dump($keys);
+                    //var_dump($this->Salvar('tr_cliente', array( 'cliente' => 'http://copcorental.solutions' )));
+
+                    //var_dump($this->Query('SELECT * FROM tr_cliente WHERE cliente = :cliente AND id_cliente = :id_cliente', array('cliente' => 'http://copcorental.solutions', 'id_cliente'=>1)));
+
+                    var_dump($this->Update('tr_cliente', array('cliente'), 'WHERE cliente = ?', array('http://copcorental.solutions')));
 		}
 	}
