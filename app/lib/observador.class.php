fe050a72024c4e027545918e9ef7e53e62f96d7c
André Matias <andrersmatias@gmail.com>
2016-06-27 01:25:16 -0300

    Classe Abstrata Observador
    Extendendo de DbTrack .
    Removendo elementos obsoletos

--- app/lib/observador.class.php
+++ app/lib/observador.class.php
@@ -1,26 +1,22 @@
 <?php
 namespace App\Lib;
 
+use App\Lib\DbTrack;
+
 /**
  * <b> Observador </b>
  * Classe abstrata para definir os padrões dos observadores das classes Sujeito
  * @author André Matias
  * @version 0.1
  */
-Abstract class Observador
+Abstract class Observador extends DbTrack
 {
-    /**
-     * Instancia da classe dbTrack
-     * @var obj dbTrack 
-     */
-    protected $database;
-
-    
+   
     /**
      * Método abstrato para obrigar as classes filhas a implementar este método
      * essencial para os observadores.
      * @param Sujeito $dados instancia da classe que esta sendo observada.
      */
     abstract public function Atualizar( Sujeito $dados );
-
+    
 }
