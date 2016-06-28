a589b9b10afa008584ca962f05341dbe8a8e5074
André Matias <andrersmatias@gmail.com>
2016-06-28 09:35:59 -0300

    Novos Testes

--- app/core/controllers/debugg.class.php
+++ app/core/controllers/debugg.class.php
@@ -8,8 +8,9 @@ namespace App\Core\Controllers;
 use \App\Lib\Cliente;
 use App\Lib\User;
 use \App\Lib\Sessao;
-use App\Lib\DbTrack AS Banco;
 use \App\Lib\Url;
+use \App\Lib\Interesse;
+use \App\Lib\Origem;
 
 class Debugg
 {
@@ -22,29 +23,32 @@ class Debugg
         $user    = new User();
         $sessao  = new Sessao();
         $url     = new Url();
+        $interesse = new Interesse();
+        $origem = new Origem();
         
         $api          = new \App\Lib\TrackingAPI();
-        $api->cliente = 'http://blogar.com.br';
-        $api->user    = 'Cardoso';
-        $api->url = 'http://bind.com.br';
+        //$api->cliente = 'http://blogar.com.br';
+        //$api->user    = 'Cardoso';
+        $api->url = 'http://bing.com.br';
         $api->shortUrl = 'Bing';
-        $api->sessionDate = '2016-06-30';
-        $api->sessionStart = '14:00:00';
-        $api->sessionEnd = '17:20:00';
-        $api->interesse = 'interesse teste';
-        $api->linhaDeNegocio = 'bl100';
-        $api->origem = 'google';
-        $api->score = '10';
+        //$api->sessionDate = '2016-06-30';
+        //$api->sessionStart = '14:00:00';
+        //$api->sessionEnd = '17:20:00';
+        //$api->interesse = 'interesse';
+        //$api->linhaDeNegocio = 'business Line';
+        //$api->origem = 'google';
+        //$api->score = '10';
 
 
-        $api->IncluirObservadores($cliente);
-        $api->IncluirObservadores($user);
-        $api->IncluirObservadores($url);
-        $api->IncluirObservadores($sessao);
-        $api->Notificar();
+        //$api->incluirObservadores($cliente);
+        //$api->incluirObservadores($user);
+        //$api->incluirObservadores($interesse);
+        //$api->incluirObservadores($origem);
+        $api->incluirObservadores($url);
+        //$api->incluirObservadores($sessao);
+        $api->notificar();
         
         echo '<pre>';
         var_dump($api);
-        //var_dump( $banco->Update($cliente->GetId(), 'tr_cliente', ['cliente' => 'http://testeDominio.com']) );
     }
 }
