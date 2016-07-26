0edccc46a174b477c44da60822e5f0e128ffbe79
André Matias <andredevelopers@gmail.com>
2016-07-26 13:34:51 -0300

    Alterações na classe model dos relatórios
    1. Removendo o método direto com o BD consolidado (Obsoleto)
    2. Documentando a classe
    3. Atribuindo novas propriedades
    4. Aplicando condicionais para mesclar o array de retorno
    5. Incluindo sistema de mesagens

--- app/core/models/results.class.php
+++ app/core/models/results.class.php
@@ -1,17 +1,23 @@
 <?php
-namespace App\Core\Models;
-
 /**
- * Description of results
- *
+ * Classe responsável por gerar os dados para a exibição do relatório consolidado
+ * e detalhado segundo o período estabelecido pelo usuário e o cliente atual
+ * @version 0.1
+ * @link andrersmatias@gmail.com
+ * @link http://github.com/andrematias
  * @author André Matias
  */
+
+namespace App\Core\Models;
+
+//Carregando as bibliotecas necessárias
 use App\Lib\Base;
 use App\Lib\Sessao;
 use App\Lib\User;
 use App\Lib\Cliente;
 use App\Lib\Origem;
 use \App\Lib\Interesse;
+
 
 class Results extends \App\Lib\Main\MainModel
 {
@@ -31,65 +37,18 @@ class Results extends \App\Lib\Main\MainModel
 
     private $origem;
 
+    private $timeNavigation;
+
+    private $totalContent;
+    
+    private $infoUser;
+
     /**
      * Método para retornar todos os dados para o relatório do tipo consolidado
-     * @return Array Associativo com todos os dados
+     * @return Array com todos os dados
      */
     public function consolidado(){
-        $sql = "SELECT
-        'Prospect' AS visitor,
-        ' ' AS description,
-        'Others' AS subject,
-        session_date AS data,
-        type_interest,
-        nome AS request,
-        email,
-        cargo AS function,
-        telefone,
-        empresa,
-        uf AS sbu,
-        endereco,
-        cep,
-        cidade,
-        estado,
-        pais,
-        cnpj,
-        source AS origem,
-        lead_type AS leadType,
-        SEC_TO_TIME(SUM(TIME_TO_SEC(TIMEDIFF(session_end, session_start)))) AS tempo_de_navegacao,
-        COUNT(id_user) as content,
-        SUM(default_score) AS score
-        FROM tr_base
-        JOIN tr_cliente USING(id_cliente)
-        JOIN tr_user USING(id_user)
-        JOIN tr_session USING (id_user)
-        JOIN tr_url using(id_url)
-        LEFT JOIN tr_type USING(id_type)
-        LEFT JOIN tr_source ON tr_source.id_source = tr_url.id_source
-        WHERE tr_cliente.cliente = :cliente
-        AND session_date BETWEEN :date_start AND :date_end
-        GROUP BY email, MONTH(session_date)
-        ORDER BY Score DESC";
-
-        $this->startDate = (filter_input(INPUT_POST, 'date') ? filter_input(INPUT_POST, 'date') : '1990-01-01');
-        $this->endDate = (filter_input(INPUT_POST, 'date_end') ? filter_input(INPUT_POST, 'date_end') : date('Y-m-d', time()));
-
-       $conn = parent::getConn();
-       $query = $conn->prepare($sql);
-
-       $params = [
-            ':cliente' => $_SESSION['cliente'],
-            ':date_start' => $this->startDate,
-            ':date_end' => $this->endDate,
-        ];
-       $query->execute($params);
-       return $query->fetchAll(\PDO::FETCH_ASSOC);
-    }
-
-    /**
-     * Teste utilizando as classes bases
-     */
-    public function consolidado2(){
+        //Classes necessárias para gerar o relatório
         $base = new Base;
         $sessao = new Sessao;
         $user = new User;
@@ -97,30 +56,68 @@ class Results extends \App\Lib\Main\MainModel
         $origem = new Origem;
         $interesse = new Interesse;
 
+        //Recupera o id do cliente atual
         $this->clienteId = $cliente->getClienteId($_SESSION['cliente']);
+        //Verifica todos os usuários deste cliente
         $this->userId = $user->allUsersFromCliente($this->clienteId);
+
+        //Captura as datas se passadas via POST
+        $this->startDate = (filter_input(INPUT_POST, 'date') ? filter_input(INPUT_POST, 'date') : '1990-01-01');
+        $this->endDate = (filter_input(INPUT_POST, 'date_end') ? filter_input(INPUT_POST, 'date_end') : date('Y-m-d', time()));
+
 
         $out = [];
 
+        /**
+         *  @var $id string
+         * Identificação do usuário
+         **/
         foreach ($this->userId as $id){
+
+            /*Atribui os valores das classes externas aos atributos da classe atual*/
+
             $this->interesses = $interesse->getUserInterest($id['id_user']);
             $this->businessLine = $interesse->getUserBusinessLine($id['id_user']);
             $this->origem = $origem->getOrigensByUserId($id['id_user']);
             $this->score = $origem->getScoreByUserId($id['id_user']);
-            
+            $this->timeNavigation = $sessao->navigationTime($id['id_user'], $this->startDate, $this->endDate);
+            $this->infoUser = $base->allFromUser($id['id_user']);
+            $this->totalContent = $sessao->countContent($id['id_user'], $this->startDate, $this->endDate);
 
-            $merge = \array_merge(
-                $sessao->navigationTime($id['id_user']),
-                $base->allFromUser($id['id_user']),
-                $sessao->countContent($id['id_user']),
-                array('interesses' => $this->interesses),
-                array('businessLine' => $this->businessLine),
-                array('origem' => $this->origem),
-                array('score' => $this->score)
-            );
+            /*Só monta o array mesclado com todas as informações se existir as
+             * informações do usuário
+             */
+            if($this->infoUser && isset($this->timeNavigation['session_date'])){
             
-            $out[] = $merge;
+                $merge = \array_merge(
+                    $this->timeNavigation,
+                    $this->infoUser,
+                    $this->totalContent,
+                    array(
+                    'interesses' => $this->interesses ,
+                    'businessLine' => $this->businessLine ,
+                    'origem' => $this->origem,
+                    'score' => $this->score,
+                    'subject' => $this->interesses,
+                    'description' => '',
+                    'visitor' => 'Prospect',
+                    )
+                );
+                //atribui os valores de merge em um array matriz
+                $out[] = $merge;
+            }
         }
+
+        /*
+         * Coloca as mensagens no buffer para ser exibida na view conforme a
+         * situação do array $out
+         */
+        if(empty($out)){
+            parent::newMessage('Não existem dados para este periodo', 'error');
+        }else{
+            parent::newMessage('Requisição realizada com sucesso', 'success');
+        }
+
         return $out;
     }
 }
